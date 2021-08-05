c9455123adf1198e8e9b39c1447a8573717c3110
harelbaruchi <os_webservice3@savion.huji.ac.il>
2021-08-05 17:25:37 +0300

    app was made functional

--- app.js
+++ app.js
@@ -4,21 +4,33 @@ const bodyParser= require('body-parser');
 
 const app=express();
 
+let items=[];
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
 
 app.get("/", function(req,res){
 
   var today= new Date();
 
-  var options={
+  let options={
     weekday: "long",
     day: "numeric",
     month: 'long'
   };
 
-  var day= today.toLocaleDateString("en-us", options);
-res.render("list", {kindOfDay: day});
+  let day= today.toLocaleDateString("en-us", options);
+res.render("list", {kindOfDay: day, newListItems: items});
 });
+
+app.post("/", function(req, res){
+  item= req.body.newItem;
+
+  items.push(item);
+res.redirect("/");
+})
 
 app.listen(3000, function(){
   console.log("server started and running on port 3000")
